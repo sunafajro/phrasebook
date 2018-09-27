93be60b4d362a6161b04acd468d0e10bd025e8d4
Evgeny Belkin <sunafajro@gmail.com>
2018-09-27 21:18:36 +0300

    изменен порт бекенда

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   filenameHashing: false,
   devServer: {
     port: 3000,
-    proxy: "http://localhost:8081"
+    proxy: "http://localhost:5000"
   },
   productionSourceMap: false
 }
