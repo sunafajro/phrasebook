c5cf5bd49455a8167c5225905f507c08873dee95
Evgeny Belkin <sunafajro@gmail.com>
2018-09-30 18:36:56 +0300

    added fontawesome

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
 
 Vue.config.productionTip = false;
 
