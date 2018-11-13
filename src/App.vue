53d61bb076b876694022aee9f20a3dd3dcf36b69
Evgeny Belkin <sunafajro@gmail.com>
2018-11-13 22:59:45 +0300

    functional improvements

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,35 @@
 <template>
   <div class="container-fluid">
-    <small>Количество фраз на сайте: <i>{{ count }}</i></small>
-    <div class="input-group mb-3">
-      <div class="input-group-prepend">
-        <button class="btn btn-info" type="button" @click="toggleLang">{{ language }}</button>
+    <div :class="'alert alert-' + status.type" v-if="!started">{{ status.text }}</div>
+    <div v-if="started">
+      <h2>{{ labels.pageTitle }}</h2>
+      <small>Количество фраз на сайте: <i>{{ count }}</i></small>
+      <div class="input-group mb-3">
+        <div class="input-group-prepend">
+          <button class="btn btn-info" type="button" @click="toggleLang">{{ current }}</button>
+        </div>
+        <input class="form-control" type="text" :value="search" @input="inputSearchText" @keyup.enter="getPhrases" />
+        <div class="input-group-append">
+          <button class="btn btn-success" type="button" @click="getPhrases"><i class="fas fa-search"></i></button>
+        </div>      
       </div>
-      <input class="form-control" type="text" :value="search" @input="inputSearchText" @keyup.enter="getPhrases" />
-      <div class="input-group-append">
-        <button class="btn btn-success" type="button" @click="getPhrases"><i class="fas fa-search"></i></button>
-      </div>      
-    </div>
-    <div>
-      <div :class="'alert alert-' + status.type" v-if="Object.keys(status).length">{{ status.text }}</div>
-      <div class="card" style="margin-bottom: 0.5rem" :key="item.id" v-for="item in phrases" v-if="phrases.length">
-        <div class="card-body">
-          <div class="text-block">
-            <div style="font-size: 18px">
-              <span style="color: #920505;font-size: 20px;font-weight: 600">{{ item.term }},</span>
-              {{ " " }}
-              <span style="color:#555">{{ item.transcription }},</span>
-              {{ " " }}
-              <span style="color: #0a6482; font-style: italic">{{ item.translation }}</span>
-            </div>
-            <div style="style: font-size: 14px">
-              <span :key="'examples-' + item.id + '-' + i" v-for="(e, i) in item.examples">
-                <span style="color:#920505">{{ e.cv }}</span> — <span style="color:#0a6482">{{ e.ru }}</span>{{ ". " }}
-              </span>
+      <div>
+        <div :class="'alert alert-' + status.type" v-if="Object.keys(status).length">{{ status.text }}</div>
+        <div class="card" style="margin-bottom: 0.5rem" :key="item.id" v-for="item in phrases" v-if="phrases.length">
+          <div class="card-body">
+            <div class="text-block">
+              <div style="font-size: 18px">
+                <span style="color: #920505;font-size: 20px;font-weight: 600">{{ item.term }},</span>
+                {{ " " }}
+                <span style="color:#555">{{ item.transcription }},</span>
+                {{ " " }}
+                <span style="color: #0a6482; font-style: italic">{{ item.translation }}</span>
+              </div>
+              <div style="style: font-size: 14px">
+                <span :key="'examples-' + item.id + '-' + i" v-for="(e, i) in item.examples">
+                  <span style="color:#920505">{{ e.cv }}</span> — <span style="color:#0a6482">{{ e.ru }}</span>{{ ". " }}
+                </span>
+              </div>
             </div>
           </div>
         </div>
@@ -39,24 +43,20 @@ import { mapActions, mapState } from "vuex";
 
 export default {
   computed: mapState([
-    "language",
-    "started",
+    "count",
+    "current",
+    "labels",
     "loading",
     "phrases",
-    "count",
     "search",
+    "started",
     "status"
   ]),
-  created() {
-    this.getPhrasesCount();
+  async created() {
+    await this.getAppState();
   },
   methods: {
-    ...mapActions([
-      "getPhrases",
-      "getPhrasesCount",
-      "setSearchText",
-      "toggleLang"
-    ]),
+    ...mapActions(["getAppState", "getPhrases", "setSearchText", "toggleLang"]),
     inputSearchText(e) {
       this.setSearchText(e.target.value);
     }
--- src/store.js
+++ src/store.js
@@ -6,21 +6,40 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    started: true,
-    labels: {},
-    language: "cv",
+    count: 0,
+    current: "cv",
+    labels: {
+      errorOccurs: {
+        text: "Произошла ошибка",
+        type: "danger"
+      },
+      notFound: {
+        text: "Совпадений не найдено",
+        type: "warning"
+      },
+      pageTitle: "500 основных чувашских корней",
+      termsCount: "Количество фраз на сайте",
+      typeSearchText: {
+        text: "Наберите произвольный текст для поиска",
+        type: "info"
+      }
+    },
+    languages: ["cv", "ru"],
     loading: false,
     phrases: [],
-    count: 0,
     search: "",
-    status: {}
+    started: false,
+    status: {
+      text: "Загрузка приложения",
+      type: "info"
+    }
   },
   mutations: {
     updateCount(state, data) {
       state.count = data;
     },
     updateLanguage(state, data) {
-      state.language = data;
+      state.current = data;
     },
     updatePhrases(state, data) {
       state.phrases = data;
@@ -28,53 +47,60 @@ export default new Vuex.Store({
     updateSearch(state, data) {
       state.search = data;
     },
-    updateStatus(state, data) {
-      state.status = data;
+    updateAppState(state, data) {
+      state.count = data.count;
+      state.labels = data.labels;
+      state.languages = data.languages;
+      state.started = true;
+      state.status = data.labels.typeSearchText;
+    },
+    updateStatus(state, { text, type }) {
+      state.status = { text, type };
     }
   },
   actions: {
+    async getAppState({ commit, state }) {
+      try {
+        const { data } = await axios.get("/state");
+        commit("updateAppState", data);
+      } catch (e) {
+        commit("updateStatus", state.labels.errorOccurs);
+      }
+    },
     async getPhrases({ commit, state }) {
       try {
         const { data } = await axios.get(
-          `/phrases?q=${state.search}&lang=${state.language}`
+          `/phrases?q=${state.search}&lang=${state.current}`
         );
         if (data.hasOwnProperty("count")) {
           commit("updateCount", parseInt(data.count, 10));
         }
-        if (data.phrases.length) {
-          commit("updatePhrases", data.phrases);
-          commit("updateStatus", {});
+        if (Array.isArray(data.phrases)) {
+          if (data.phrases.length) {
+            commit("updatePhrases", data.phrases);
+            commit("updateStatus", {});
+          } else {
+            commit("updatePhrases", []);
+            commit("updateStatus", state.labels.notFound);
+          }
         } else {
           commit("updatePhrases", []);
-          commit("updateStatus", {
-            text: state.labels.notFound,
-            type: "warning"
-          });
+          commit("updateStatus", state.labels.typeSearchText);
         }
       } catch (e) {
         commit("updatePhrases", []);
-        commit("updateStatus", {
-          text: state.labels.errorOccurs,
-          type: "danger"
-        });
-      }
-    },
-    async getPhrasesCount({ commit, state }) {
-      try {
-        const { data } = await axios.get("/phrases/count");
-        commit("updateCount", parseInt(data.count, 10));
-      } catch (e) {
-        commit("updateStatus", {
-          text: state.labels.errorOccurs,
-          type: "danger"
-        });
+        commit("updateStatus", state.labels.errorOccurs);
       }
     },
     setSearchText({ commit }, payload) {
       commit("updateSearch", payload);
     },
     toggleLang({ commit, state }) {
-      commit("updateLanguage", state.language === "cv" ? "ru" : "cv");
+      const i = state.languages.indexOf(state.current);
+      commit(
+        "updateLanguage",
+        i === 0 ? state.languages[1] : state.languages[0]
+      );
     }
   }
 });
