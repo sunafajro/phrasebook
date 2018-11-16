10067686f87a291d128511a04eb290e64f17f7a4
Evgeny Belkin <sunafajro@gmail.com>
2018-11-17 00:11:14 +0300

    old header splitted to two components

--- src/Header.vue
+++ src/Header.vue
@@ -1,30 +1,13 @@
-
 <template>
   <div>
     <h2>{{ labels.pageTitle.text }}</h2>
-    <small>{{ labels.termsCount.text }}: <i>{{ totalCount }}</i></small>
-    <div class="input-group mb-3">
-      <div class="input-group-prepend">
-        <button class="btn btn-info" type="button" @click="toggleLang">{{ current }}</button>
-      </div>
-      <input class="form-control" type="text" :value="search" @input="inputSearchText" @keyup.enter="startSearch" />
-      <div class="input-group-append">
-        <button class="btn btn-success" type="button" @click="startSearch"><i class="fas fa-search"></i></button>
-      </div>      
-    </div>
   </div>
 </template>
 
 <script>
-import { mapActions, mapState } from "vuex";
+import { mapState } from "vuex";
 
 export default {
-  computed: mapState(["current", "labels", "search", "totalCount"]),
-  methods: {
-    ...mapActions(["getPhrases", "setSearchText", "startSearch", "toggleLang"]),
-    inputSearchText(e) {
-      this.setSearchText(e.target.value);
-    }
-  }
+  computed: mapState(["labels"])
 };
 </script>
---
+++ src/Search.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <small>{{ labels.termsCount.text }}: <i>{{ totalCount }}</i></small>
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <button class="btn btn-info" type="button" @click="toggleLang">{{ current }}</button>
+      </div>
+      <input class="form-control" type="text" :value="search" @input="inputSearchText" @keyup.enter="startSearch" />
+      <div class="input-group-append">
+        <button class="btn btn-success" type="button" @click="startSearch"><i class="fas fa-search"></i></button>
+      </div>      
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapState } from "vuex";
+
+export default {
+  computed: mapState(["current", "labels", "search", "totalCount"]),
+  methods: {
+    ...mapActions(["getPhrases", "setSearchText", "startSearch", "toggleLang"]),
+    inputSearchText(e) {
+      this.setSearchText(e.target.value);
+    }
+  }
+};
+</script>
