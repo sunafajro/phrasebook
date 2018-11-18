0f1a4e772572f36345d3cc997e4801d5f9656a39
Evgeny Belkin <sunafajro@gmail.com>
2018-11-18 22:16:44 +0300

    added routes for new components

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Dictionary from "./Dictionary.vue";
+import RandomPhrase from "./RandomPhrase.vue";
+import About from "./About.vue";
 
 Vue.use(Router);
 
@@ -10,6 +12,16 @@ export default new Router({
       path: "/",
       name: "Dictionary",
       component: Dictionary
+    },
+    {
+      path: "/random",
+      name: "RandomPhrase",
+      component: RandomPhrase
+    },
+    {
+      path: "/about",
+      name: "About",
+      component: About
     }
   ]
 });
