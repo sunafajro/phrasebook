1919beda04270236c7daeb0d473ebe0ba068876f
Evgeny Belkin <sunafajro@gmail.com>
2018-11-19 23:16:32 +0300

    two buttons for sending letter to site admin

--- src/About.vue
+++ src/About.vue
@@ -10,16 +10,17 @@
         <div class="card">
           <div class="card-body" v-html="about[languages[0]]"></div>
         </div>
+        <div class="text-center" style="margin-top: 0.5rem">
+          <button class="btn btn-success">Ҫыру ҫыр</button>
+        </div>
       </div>
       <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6">
         <div class="card">
           <div class="card-body text-justify" v-html="about[languages[1]]"></div>
         </div>
-      </div>
-    </div>
-    <div class="row" style="margin-top: 0.5rem">
-      <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 text-center">
-        <button class="btn btn-success">Написать письмо</button>
+        <div class="text-center" style="margin-top: 0.5rem">
+          <button class="btn btn-success">Написать письмо</button>
+        </div>
       </div>
     </div>
   </div>
