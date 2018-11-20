bf7f60355041d7f36cab885d4c4d51509a9a7564
Evgeny Belkin <sunafajro@gmail.com>
2018-11-21 00:09:35 +0300

    unnecessary action removed

--- src/Search.vue
+++ src/Search.vue
@@ -3,9 +3,9 @@
     <small>{{ labels.termsCount.text }}: <i>{{ totalCount }}</i></small>
     <div class="input-group mb-3">
       <div class="input-group-prepend">
-        <button class="btn btn-info" type="button" @click="toggleLang">{{ current }}</button>
+        <button class="btn btn-info" type="button" @click="updateLanguage">{{ current }}</button>
       </div>
-      <input class="form-control" type="text" :value="search" @input="inputSearchText" @keyup.enter="startSearch" />
+      <input class="form-control" type="text" v-model="search" @keyup.enter="startSearch" />
       <div class="input-group-append">
         <button class="btn btn-success" type="button" @click="startSearch"><i class="fas fa-search"></i></button>
       </div>      
@@ -14,15 +14,23 @@
 </template>
 
 <script>
-import { mapActions, mapState } from "vuex";
+import { mapActions, mapMutations, mapState } from "vuex";
 
 export default {
-  computed: mapState(["current", "labels", "search", "totalCount"]),
-  methods: {
-    ...mapActions(["getPhrases", "setSearchText", "startSearch", "toggleLang"]),
-    inputSearchText(e) {
-      this.setSearchText(e.target.value);
+  computed: {
+    ...mapState(["current", "labels", "totalCount"]),
+    search: {
+      get() {
+        return this.$store.state.search;
+      },
+      set(value) {
+        this.$store.commit("updateSearch", value);
+      }
     }
+  },
+  methods: {
+    ...mapActions(["getPhrases", "startSearch", "toggleLang"]),
+    ...mapMutations(["updateLanguage"])
   }
 };
 </script>
